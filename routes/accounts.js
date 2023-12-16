fa505da2150ea8baf5a4a9d91d556c760a58169d
Judson Cabral de Melo <judsoncabral1@hotmail.com>
2023-12-15 22:30:50 -0300

    feature: delete account

--- routes/accounts.js
+++ routes/accounts.js
@@ -20,26 +20,39 @@ router.post("/", async (req, res) => {
   }
 });
 
-
 router.get("/", async (req, res) => {
-    try {
-      const data = JSON.parse(await readFile(global.fileName));
-      delete data.nextId;
-      res.send(data);
-    } catch (err) {
-      res.status(400).send({ error: err.message });
-    } finally {
-      res.end();
-    }
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextId;
+    res.send(data);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  } finally {
+    res.end();
+  }
 });
 
 router.get("/:id", async (req, res) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     const account = data.accounts.find(
-      account => account.id === parseInt(req.params.id)
+      (account) => account.id === parseInt(req.params.id)
     );
     res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  } finally {
+    res.end();
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
   } catch (err) {
     res.status(400).send({ error: err.message });
   } finally {
